01aeee3d40da234a853ca125c07320db5b21350d
cde-la-r <cde-la-r@student.42.fr>
2024-11-05 14:54:30 +0100

    tables, graphs, code

---
+++ sample.m
@@ -0,0 +1,13 @@
+% sample.m - Cálculo de la raíz cuadrada de cada elemento en un vector
+
+% Definición del vector
+vec = [1, 4, 9, 16, 25];
+
+% Cálculo de la raíz cuadrada
+sqrt_vec = sqrt(vec);
+
+% Mostrar resultados
+disp('Vector original:')
+disp(vec)
+disp('Raíz cuadrada de cada elemento:')
+disp(sqrt_vec)
